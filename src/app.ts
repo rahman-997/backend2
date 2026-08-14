9cd38e610136f11a1cd5d567de9617708826bca7
abdulrahman hajar <316491264+rahman-997@users.noreply.github.com>
2026-08-14 10:09:26 +0300

    feat: add venues CRUD resource

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import routes from "./routes/index.js";
+import { errorHandler } from "./middleware/errorHandler.js";
+
+const app = express();
+app.use(express.json());
+app.use(routes);
+app.use(errorHandler);
+
+export default app;
